aacd0531faa764ba461517f0ae8a28a3e30e5956
swapnil <swap81733@gmail.com>
2020-10-23 16:39:38 +0530

    Initial Commit

---
+++ 9.QuizllerApp/Controller/ViewControllers.swift
@@ -0,0 +1,28 @@
+//
+//  ViewControllers.swift
+//  9.QuizllerApp
+//
+//  Created by swapnil jadhav on 23/10/20.
+//  Copyright © 2020 t. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllers: UIViewController {
+
+    @IBOutlet weak var questionLbl: UILabel!
+    @IBOutlet weak var progressLbl: UILabel!
+    @IBOutlet weak var scoreLbl: UILabel!
+    
+    @IBOutlet weak var preogressBar: UIProgressView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+   
+}
